b73270eb504d20954d1f84f29f62711d66e704e3
deltd3v <deltd3v@gmail.com>
2021-10-17 16:02:14 +0300

    😎

---
+++ script.js
@@ -0,0 +1,16 @@
+const el = {
+	body: document.querySelector('body'),
+
+	navLogo: document.querySelector('.nav-bar__logo'),
+	navMenu: document.querySelector('#nav-bar__mobile-menu'),
+	navMenuLinks: document.querySelector('.nav-bar__menu'),
+};
+
+const toggleMobileMenu = () => {
+	console.log('menu-toggled');
+	el.body.classList.toggle('active');
+	el.navMenu.classList.toggle('active');
+	el.navMenuLinks.classList.toggle('active');
+};
+
+el.navMenu.addEventListener('click', toggleMobileMenu);
